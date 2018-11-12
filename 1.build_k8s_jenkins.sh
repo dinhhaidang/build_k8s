5623411a9864370ff949ffee6e51dcc0aa95df9d
dinhhaidang <dinhaidang@gmail.com>
2018-11-12 11:14:13 +0700

    Update 1.build_k8s_jenkins.sh

--- 1.build_k8s_jenkins.sh
+++ 1.build_k8s_jenkins.sh
@@ -4,23 +4,17 @@
 # 1. Setup K8S tren GCP
 # 2. Setup jenskin. authencication
 # 3. 
-
+gcloud config set compute/zone asia-east1-a
 #----------------Setup moi truong K8S--------------------#
 #Cau hinh zone mac dinh tren GCP
 echo "#-------------------------------------------------------------------#"
 echo "#                 Tao VM Rancher quan ly Cluster                    #"
 echo "#-------------------------------------------------------------------#"
-gcloud config set compute/zone asia-east1-a
 gcloud compute instances create rancher-k8s \
    --custom-cpu 1 \
    --custom-memory 2 \
    --boot-disk-size 10 \
    --boot-disk-type pd-ssd \
-
-echo "#-------------------------------------------------------------------#"
-echo "#                 Dang tao network: ecoe-k8s                        #"
-echo "#-------------------------------------------------------------------#"
-gcloud compute networks create ecoe-k8s
 
 #Tao he thong K8S
 echo "#-------------------------------------------------------------------#"
@@ -28,7 +22,6 @@ echo "#                 Dang tao cluster kubernetes                       #"
 echo "#-------------------------------------------------------------------#"
 gcloud beta container clusters create ecoe-k8s \
 --num-nodes 3 \
---network ecoe-k8s \
 --machine-type n1-standard-2 \
 --disk-type pd-ssd \
 --disk-size 10 \
@@ -60,35 +53,14 @@ kubectl patch storageclass standard -p '{"metadata": {"annotations":{"storagecla
 kubectl patch storageclass faster-ssd -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 echo "\n"
 
-
-echo "#-------------------------------------------------------------------#"
-echo "#         Chuan bi setup jenkins, se bat dau trong 1 phut...        #"
-echo "#-------------------------------------------------------------------#"
-sleep 1m
-echo "#-----------------Setup Jenkins----------------------#"
-helm install -n cd stable/jenkins -f jenkins/values.yaml --version 0.16.6 --wait
-echo "\n"
-
 echo "----------------Setup Cockpit - Visual Pod on K8S-----------------------"
 kubectl create namespace cockpit-demo
 kubectl create -f cockpit.json -n cockpit-demo
 echo "\n"
 
-echo "#----------------Thong tin truy cap Jenkins UI------------------#"
-kubectl get svc
-echo "\n"
-
-echo "#------------------Mat khau dang nhap Jenkins-----------------------#"
-printf $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
-echo "#-------------------------------------------------------------------#"
-echo "\n"
-
 echo "#-----------------Thong tin web UI Cockpit-------------------#"
 kubectl get svc -n cockpit-demo
 echo "\n"
-
-export POD_NAME=$(kubectl get pods -l "component=cd-jenkins-master" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward $POD_NAME 8080:8080 >> /dev/null &
 
 echo "#-------------------------------------------------------------------#"
 echo "#            HE THONG DA HOAN THANH XAY DUNG MOI TRUONG!            #"
