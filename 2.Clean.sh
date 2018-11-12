23fafffd94224c327b1c377c26a364dcb78ff973
dinhhaidang <dinhaidang@gmail.com>
2018-11-12 11:10:18 +0700

    Update 2.Clean.sh

--- 2.Clean.sh
+++ 2.Clean.sh
@@ -3,3 +3,8 @@ echo "#                 Dang xoa cluster kubernetes.....                  #"
 echo "#-------------------------------------------------------------------#"
 printf "y\n" | gcloud container clusters delete ecoe-k8e || true
 
+echo "#-------------------------------------------------------------------#"
+echo "#                      Dang xoa VM Rancher.....                     #"
+echo "#-------------------------------------------------------------------#"
+printf "y\n" | gcloud compute instances delete rancher-k8s || true
+
